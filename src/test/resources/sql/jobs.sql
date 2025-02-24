ba012ab98e8fda0b26bdd31c276839206fab8252
Andrei Dumitrescu <andrei.r.dumitrescu@gmail.com>
2025-02-24 11:25:51 +0200

    Testing Jobs

---
+++ src/test/resources/sql/jobs.sql
@@ -0,0 +1,62 @@
+CREATE TABLE jobs
+(
+    id          uuid             DEFAULT gen_random_uuid(),
+    date        bigint  NOT NULL,
+    ownerEmail  text    NOT NULL,
+    company     text    NOT NULL,
+    title       text    NOT NULL,
+    description text    NOT NULL,
+    externalUrl text    NOT NULL,
+    remote      boolean NOT NULL default false,
+    location    text    NOT NULL,
+    salaryLo    integer,
+    salaryHigh  integer,
+    currency    text,
+    country     text,
+    tags        text[],
+    image       text,
+    seniority   text,
+    other       text,
+    active      boolean NOT NULL default FALSE
+);
+
+ALTER TABLE jobs
+    add constraint pk_jobs primary key (id);
+
+
+INSERT INTO jobs (id,
+                  date,
+                  ownerEmail,
+                  company,
+                  title,
+                  description,
+                  externalUrl,
+                  remote,
+                  location,
+                  salaryLo,
+                  salaryHigh,
+                  currency,
+                  country,
+                  tags,
+                  image,
+                  seniority,
+                  other,
+                  active)
+values ('843df718-ec6e-4d49-9289-f799c0f40064',
+        1659186086,
+        'daniel@rockthejvm.com',
+        'Awesome Company',
+        'Tech Lead',
+        'An awesome job in Berlin',
+        'https://rockthejvm.com/awesomejob',
+        false,
+        'Berlin',
+        2000,
+        3000,
+        'EUR',
+        'Germany',
+        ARRAY ['scala', 'scala-3', 'cats' ],
+        NULL,
+        'Senior',
+        NULL,
+        FALSE)
